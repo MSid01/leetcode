1b6d48845ca2fbaaab4605f7c9c38bf93bb188dd
igor <climberig@gmail.com>
2021-02-13 12:05:17 -0800

    solutions

---
+++ src/s1742/Solution.java
@@ -0,0 +1,16 @@
+package s1742;
+
+public class Solution {
+    public int countBalls(int lo, int hi) {
+        int boxes[] = new int[46], max = 0;
+        for (int i = lo; i <= hi; i++) {
+            int sum = 0, n = i;
+            while (n > 0) {
+                sum += n % 10;
+                n /= 10;
+            }
+            max = Math.max(max, ++boxes[sum]);
+        }
+        return max;
+    }
+}
